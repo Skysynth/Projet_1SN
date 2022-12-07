a79c2be3411a96bac2e210c69a9dc6a2700e5088
juleelee <119613908+juleelee@users.noreply.github.com>
2022-12-07 11:15:47 +0100

    module table de routage

---
+++ sda_exceptions.ads
@@ -0,0 +1,7 @@
+-- D�finition d'une exception commune � toutes les SDA.
+package SDA_Exceptions is
+
+	Cle_Absente_Exception  : Exception;
+
+
+end SDA_Exceptions;
--- table_routage.adb
+++ table_routage.adb
@@ -1,1 +1,148 @@
-with
+with Ada.Text_IO;            use Ada.Text_IO;
+with SDA_Exceptions;         use SDA_Exceptions;
+with Ada.Unchecked_Deallocation;
+
+package body LCA is
+
+	procedure Free is
+		new Ada.Unchecked_Deallocation (Object => T_Cellule, Name => T_LCA);
+
+
+	procedure Initialiser(Sda: out T_LCA) is
+	begin
+		Sda:=null;
+			
+	end Initialiser;
+
+
+	function Est_Vide (Sda : T_LCA) return Boolean is
+	begin
+	
+		return Sda = null;	
+	end;
+
+
+	function Taille (Sda : in T_LCA) return Integer is
+	
+	begin
+		
+		if Sda = null then 
+			return 0;
+		else 
+			return Taille(Sda.all.Suivant) + 1;
+		end if;
+	
+
+	end Taille;
+
+
+	procedure Enregistrer (Sda : in out T_LCA ; Cle : in T_Cle ; Donnee : in T_Donnee) is
+
+	
+
+	begin
+		
+		if Est_Vide(Sda) then
+            Sda := new T_Cellule'(Cle, Donnee, Sda);
+        elsif Sda.all.Cle=Cle then
+            Sda.all.Donnee := Donnee;
+        else
+            Enregistrer(Sda.all.Suivant, Cle, Donnee);
+        end if;
+
+	end Enregistrer;
+
+--Savoir si une clé est présente dans une SDA.
+	function Cle_Presente (Sda : in T_LCA ; Cle : in T_Cle) return Boolean is
+
+	
+	begin	
+		
+
+		if not(Est_Vide(Sda)) then 
+			if Sda.all.Cle = Cle then 
+				return true;
+			else 
+				return Cle_Presente(Sda.all.Suivant, Cle);
+			
+			end if;
+
+		else 
+			return false;
+
+
+		end if; 
+
+
+	end Cle_Presente;
+
+
+	function La_Donnee (Sda : in T_LCA ; Cle : in T_Cle) return T_Donnee is
+	
+	begin
+		if not( Est_Vide(Sda)) then 
+			if Sda.all.Cle = Cle then 
+				return Sda.all.Donnee;
+			
+			else 
+				return La_Donnee(Sda.all.Suivant, Cle);
+			end if;
+		else 
+			raise Cle_Absente_Exception;
+
+		end if;
+	
+	end La_Donnee;
+
+
+	procedure Supprimer (Sda : in out T_LCA ; Cle : in T_Cle) is
+	Sda0 : T_LCA;
+	begin
+		if Est_Vide(Sda) then
+            raise Cle_Absente_Exception;
+        elsif Sda.all.Cle = Cle then
+            Sda0 := Sda;
+            Sda:= Sda.all.Suivant;
+            Free(Sda0);
+        else
+            Supprimer(Sda.all.Suivant, Cle);
+        end if;
+	end Supprimer;
+
+
+	procedure Vider (Sda : in out T_LCA) is
+	begin
+		if not(Est_Vide(Sda)) then
+			Vider(Sda.all.Suivant); -- .all permet d'accèder au contenu de l'adresse que pointe le pointeur  
+			Free (Sda);
+		else
+			Null;
+		end if;
+		
+	end Vider;
+
+
+	procedure Pour_Chaque (Sda : in T_LCA) is
+	
+	begin
+		if not( Est_Vide(Sda)) then 
+			begin 
+				Traiter(Sda.all.Cle, Sda.all.Donnee);
+			exception 
+				when others => 
+					put_line(" erreur de traitement ");
+			end;
+			
+			Pour_Chaque(Sda.all.Suivant);
+			
+		else 
+			null;
+		end if;
+		
+		
+		
+			
+	end Pour_Chaque;
+
+
+end LCA;
