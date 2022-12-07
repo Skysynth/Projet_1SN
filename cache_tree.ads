a2ef38f0ec6f461d5c21d04d89bb7ebdade79c96
atrtdeee <trotelalexandre@proton.me>
2022-12-07 11:23:55 +0100

    adding spaces

--- cache_tree.ads
+++ cache_tree.ads
@@ -11,12 +11,12 @@ private
     type T_Cache is access T_Cache_Cellule;
 
     type T_Cache_Cellule is record
-        Taille: Integer;
-        Adresse: T_Adresse_IP;
-        Masque: T_Adresse_IP;
-        Interface: T_Interface;
-        Gauche: T_Cache;
-        Droit: T_Cache;
+        Taille : Integer;
+        Adresse : T_Adresse_IP;
+        Masque : T_Adresse_IP;
+        Interface : T_Interface;
+        Gauche : T_Cache;
+        Droit : T_Cache;
     end record;
 
 end cache_tree;
