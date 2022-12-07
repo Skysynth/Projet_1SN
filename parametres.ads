2e1e65b78e6f3896db48eddc87bce8d5a3f79030
atrtdeee <trotelalexandre@proton.me>
2022-12-07 10:50:57 +0100

    adding private section

--- parametres.ads
+++ parametres.ads
@@ -2,6 +2,22 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 package PARAMETRES is
 
+    type T_Politique is limited private;
+
+    type T_Param is limited private;
+
+    type T_Adresse_IP is private;
+
+    -- function is_Param_Valid(Parametre : Character) return Boolean;
+
+    -- function is_File_Valid(file : String) return Boolean;
+
+    function Initialiser return T_Param;
+
+    procedure Afficher_Param(param : T_Param);
+
+private
+
     type T_Politique is (FIFO, LRU, LFU);
 
     type T_Param is record
@@ -18,14 +34,5 @@ package PARAMETRES is
     end record;
 
     type T_Adresse_IP is mod 2 ** 32;
-
-
-    -- function is_Param_Valid(Parametre : Character) return Boolean;
-
-    -- function is_File_Valid(file : String) return Boolean;
-
-    function Initialiser return T_Param;
-
-    procedure Afficher_Param(param : T_Param);
 
 end PARAMETRES;
