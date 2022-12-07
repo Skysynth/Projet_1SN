3b3445bfa377cc205602a03cab9ec5995982c396
Skysynth <Skysynth31@gmail.com>
2022-12-07 21:44:07 +0100

    Removed unknown characters & initialisation of T_Table_Routage

--- table_routage.adb
+++ table_routage.adb
@@ -1,149 +1,190 @@
 with Ada.Text_IO;            use Ada.Text_IO;
-with SDA_Exceptions;         use SDA_Exceptions;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Unchecked_Deallocation;
+with tools; use tools;
 
+-- R1 : Concevoir et initialiser un routeur
 package body Table_Routage is
+    
+    procedure Free is
+        new Ada.Unchecked_Deallocation (Object => T_Cellule, Name => T_Table_Routage);
+    
+    function Convert_Unbounded_String_To_T_Adresse_IP(ligne : Unbounded_String) return T_Adresse_IP is
+        Adresse_Converti : T_Adresse_IP := 0;
+        mot : Unbounded_String;
+        N : Integer;
+        j : Integer := 1;
+    begin
+            
+        for i in 0..3 loop 
+            mot := Null_Unbounded_String;
+                
+            N := length(ligne);
+                
+                
+            while j <= N and then Element(ligne, j) /= '.' loop
+                
+                mot := mot & Element(ligne, j);
+                j := j+1;
+                    
+            end loop;
+                
+            -- Enregistrer la destination et le masque une fois converti dans le routeur et enregistrer l'interface dans le routeur
+              
+            Adresse_Converti := Adresse_Converti + T_Adresse_IP'Value(To_String(mot)) * (2 ** (24-8*i));
+            
+            j := j + 1;
+            
+        end loop;
+            
+        return Adresse_Converti;
+            
+    end;
+    
+    procedure Afficher(Table_Routage : in T_Table_Routage) is
+        
+        table_temp : T_Table_Routage := Table_Routage;
+    begin
+        
+        while table_temp /= null loop
+            
+            Put_Line("Adresse : " & T_Adresse_IP'Image(table_temp.all.Adresse) 
+                     & "   Masque : " & T_Adresse_IP'Image(table_temp.all.Masque) 
+                     & "   Interface : " & To_String(table_temp.all.Sortie)); 
+            
+            table_temp := table_temp.all.Suivant;
+        end loop;
+        
+    end;
+    
+    -- R2 : Initialiser un routeur 
+    procedure Initialiser(param : in T_Param; Table_Routage: out T_Table_Routage) is
+        File : File_Type;
+        Donnee : array(1..3) of Unbounded_String;
+    
+        -- Concevoir le tableau de taille 3
+        iterateur : Integer;
+        ligne : Unbounded_String;
+        
+        mot : Unbounded_String;
+        N : Integer;
+        
+        table_routage_temp : T_Table_Routage;
+        
+    begin
+        Open (File => File,
+              Mode => In_File,
+              Name => To_String(param.file_table_routage));
+        
+        -- Concevoir une nouvelle cellule dans le routeur pour stocker les donnees de la prochaine ligne 
+        Table_Routage := new T_Cellule;
+        table_routage_temp := Table_Routage;
+        
+        While not End_Of_File (File) Loop
+            
+            ligne := To_Unbounded_String(Get_Line(File));
+            N := length(ligne);
+            iterateur := 1;
+            
+            -- Concevoir un tableau de taille 3 qui va stocker des chaines de caracteres
+            
+            for j in 1..3 loop
+                
+                Donnee(j) := Null_Unbounded_String;
+                
+                
+                while iterateur <= N and then Element(ligne, iterateur) /= ' ' loop
+                    
+                    Donnee(j) := Donnee(j) & Element(ligne, iterateur);
+                    
+                    iterateur := iterateur + 1;
+                    
+                end loop;
+                iterateur := iterateur + 1;
+                   
+            end loop;
+            
+            -- Convertir l'adresse IP de la destination et du masque
+            table_routage_temp.all.Adresse := Convert_Unbounded_String_To_T_Adresse_IP(Donnee(1));
+            table_routage_temp.all.Masque := Convert_Unbounded_String_To_T_Adresse_IP(Donnee(2));
+            table_routage_temp.all.Sortie := Donnee(3);
+                        
+            if not End_Of_File (File) then
+                table_routage_temp.Suivant := new T_Cellule;
+                table_routage_temp := table_routage_temp.Suivant;
+            else
+                null;
+            end if;
+                 
+        end loop;
+        
+        Close (File);   
+        
+    end Initialiser;
 
-	procedure Free is
-		new Ada.Unchecked_Deallocation (Object => T_Cellule, Name => T_LCA);
 
-
-	procedure Initialiser(Sda: out T_LCA) is
-	begin
-		Sda:=null;
-			
-	end Initialiser;
-
-
-	function Est_Vide (Sda : T_LCA) return Boolean is
-	begin
+    function Est_Vide (Table_Routage : in T_Table_Routage) return Boolean is
+    begin
 	
-		return Sda = null;	
-	end;
+        return Table_Routage = null;	
+    end;
 
-
-	function Taille (Sda : in T_LCA) return Integer is
+    function Taille (Table_Routage : in T_Table_Routage) return Integer is
 	
-	begin
+    begin
 		
-		if Sda = null then 
-			return 0;
-		else 
-			return Taille(Sda.all.Suivant) + 1;
-		end if;
-	
-
-	end Taille;
-
-
-	procedure Enregistrer (Sda : in out T_LCA ; Cle : in T_Cle ; Donnee : in T_Donnee) is
-
-	
-
-	begin
-		
-		if Est_Vide(Sda) then
-            Sda := new T_Cellule'(Cle, Donnee, Sda);
-        elsif Sda.all.Cle=Cle then
-            Sda.all.Donnee := Donnee;
-        else
-            Enregistrer(Sda.all.Suivant, Cle, Donnee);
+        if Table_Routage = null then 
+            return 0;
+        else 
+            return Taille(Table_Routage.all.Suivant) + 1;
         end if;
-
-	end Enregistrer;
-
---Savoir si une clé est présente dans une SDA.
-	function Cle_Presente (Sda : in T_LCA ; Cle : in T_Cle) return Boolean is
-
 	
-	begin	
+
+    end Taille;
+
+
+    procedure Enregistrer (Table_Routage : in out T_Table_Routage ; adresse : T_Adresse_IP) is
+
+    begin
 		
+        null;
+    end Enregistrer;
 
-		if not(Est_Vide(Sda)) then 
-			if Sda.all.Cle = Cle then 
-				return true;
-			else 
-				return Cle_Presente(Sda.all.Suivant, Cle);
-			
-			end if;
+    --Savoir si une clé est présente dans une SDA.
+    function Cle_Presente (Table_Routage : in T_Table_Routage ; adresse : T_Adresse_IP) return Boolean is
 
-		else 
-			return false;
+    begin	
+        return false;
 
-
-		end if; 
+    end Cle_Presente;
 
 
-	end Cle_Presente;
-
-
-	function La_Donnee (Sda : in T_LCA ; Cle : in T_Cle) return T_Donnee is
+    function La_Donnee (Table_Routage : in T_Table_Routage ; adresse : T_Adresse_IP) return T_Adresse_IP is
 	
-	begin
-		if not( Est_Vide(Sda)) then 
-			if Sda.all.Cle = Cle then 
-				return Sda.all.Donnee;
-			
-			else 
-				return La_Donnee(Sda.all.Suivant, Cle);
-			end if;
-		else 
-			raise Cle_Absente_Exception;
-
-		end if;
+    begin
+        return 0;
 	
-	end La_Donnee;
+    end La_Donnee;
 
 
-	procedure Supprimer (Sda : in out T_LCA ; Cle : in T_Cle) is
-	Sda0 : T_LCA;
-	begin
-		if Est_Vide(Sda) then
-            raise Cle_Absente_Exception;
-        elsif Sda.all.Cle = Cle then
-            Sda0 := Sda;
-            Sda:= Sda.all.Suivant;
-            Free(Sda0);
-        else
-            Supprimer(Sda.all.Suivant, Cle);
-        end if;
-	end Supprimer;
+    procedure Supprimer (Table_Routage : in out T_Table_Routage ; adresse : T_Adresse_IP) is
+    begin
+        null;
+    end Supprimer;
 
 
-	procedure Vider (Sda : in out T_LCA) is
-	begin
-		if not(Est_Vide(Sda)) then
-			Vider(Sda.all.Suivant); -- .all permet d'accèder au contenu de l'adresse que pointe le pointeur  
-			Free (Sda);
-		else
-			Null;
-		end if;
-		
-	end Vider;
+    procedure Vider (Table_Routage : in out T_Table_Routage) is
+    begin
+        null;
+    end Vider;
 
 
-	procedure Pour_Chaque (Sda : in T_LCA) is
+    procedure Pour_Chaque (Table_Routage : in T_Table_Routage) is
 	
-	begin
-		if not( Est_Vide(Sda)) then 
-			begin 
-				Traiter(Sda.all.Cle, Sda.all.Donnee);
-			exception 
-				when others => 
-					put_line(" erreur de traitement ");
-			end;
+    begin
+        null;
 			
-			Pour_Chaque(Sda.all.Suivant);
-			
-		else 
-			null;
-		end if;
-		
-		
-		
-			
-	end Pour_Chaque;
+    end Pour_Chaque;
 
 
 end Table_Routage;
