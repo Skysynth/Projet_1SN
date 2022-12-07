644467eb8b7b9b1004d9a1334a1f8a4a1e6d57df
atrtdeee <trotelalexandre@proton.me>
2022-12-07 11:43:59 +0100

    adding some procedures to cache_tree

--- cache_tree.adb
+++ cache_tree.adb
@@ -1,1 +1,37 @@
-with
+-- with Ada.Text_IO; use Ada.Text_IO;
+with SDA_Exceptions; use SDA_Exceptions;
+with Ada.Unchecked_Deallocation;
+
+package body cache_tree is
+
+	procedure Free is
+		new Ada.Unchecked_Deallocation (Object => T_Cellule, Name => T_Cache);
+
+	procedure Initialiser (Sda: out T_Cache) is
+	begin
+		Cache := Null;
+	end Initialiser;
+
+
+	function Est_Vide (Sda : Cache) return Boolean is
+	begin
+		return (Cache = Null);
+	end;
+
+
+	function Taille (Sda : in T_LCA) return Integer is
+		Result: Integer;
+	begin
+		if Est_Vide (Sda) then
+			-- Le pointeur est vide donc sa taille vaut 0
+		 	Result := 0;
+		else
+			-- Récursivité sur la taille en ajoutant + 1 à chaque itération
+			Result := Taille (Sda.All.Suivant) + 1;
+		end if;
+
+		return Result;
+	end Taille;
+
+
+end cache_tree;
--- cache_tree.ads
+++ cache_tree.ads
@@ -4,6 +4,14 @@ package cache_tree is
 
     type T_Cache is limited private;
 
+    procedure Initialiser(Cache: out T_Cache) with
+        post Est_Vide(Cache);
+
+    function Est_Vide (Sda : T_LCA) return Boolean;
+
+    function Taille (Sda : in T_LCA) return Integer with
+		Post => Taille'Result >= 0 and (Taille'Result = 0) = Est_Vide (Sda);
+
 private
 
     type T_Cache_Cellule;
