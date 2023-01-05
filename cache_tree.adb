841b64a1bec09d9d4d9da9522a2032ce4744e1e4
atrtdeee <trotelalexandre@proton.me>
2023-01-05 11:18:43 +0100

    grosse modifications d'architecture du type T_Cache_Arbre

--- cache_tree.adb
+++ cache_tree.adb
@@ -5,42 +5,48 @@ with Ada.Unchecked_Deallocation;
 package body cache_tree is
 
 	procedure Free is
-		new Ada.Unchecked_Deallocation(Object => T_Cache_Cellule, Name => T_Cache_Arbre);
+		new Ada.Unchecked_Deallocation(Object => T_Cache_Cellule, Name => T_Arbre);
 
-	procedure Initialiser(Cache : out T_Cache_Arbre) is
+	procedure Initialiser(Cache : out T_Cache_Arbre; Taille : in Integer) is
 	begin
-		Cache := Null;
+		Cache.Arbre := Null;
+		Cache.Taille := Taille;
 	end Initialiser;
 
-	function Est_Vide(Cache : in T_Cache_Arbre) return Boolean is
+	function Est_Vide(Arbre : in T_Arbre) return Boolean is
 	begin
-		return (Cache = Null);
+		return (Arbre = Null);
 	end;
 
-    procedure Vider(Cache : in out T_Cache_Arbre) is
+    procedure Vider(Arbre : in out T_Arbre) is
 	begin
-        if not Est_Vide(Cache) then
+        if not Est_Vide(Arbre) then
             -- Si le cache n'est pas vide
-			Vider(Cache.All.Gauche);
-            Vider(Cache.All.Droite);
-            Free(Cache);
+			Vider(Arbre.All.Gauche);
+            Vider(Arbre.All.Droite);
+            Free(Arbre);
         else
 			-- Si le cache est vide
             Put_Line("Le cache est pas vide. Pas besoin de le vider.");
         end if;
 	end Vider;
 
-	procedure Enregistrer(Cache : in out T_Cache_Arbre; Adresse : in T_Adresse_IP; Masque : T_Adresse_IP; Sortie : Unbounded_String; Taille : Integer) is
-		Compteur_Taille : T_Cache_Arbre;
+	function Taille(Cache : in T_Cache_Arbre) return Integer is
 	begin
-		-- On initialise le compteur pour la taille
-		Compteur_Taille := Cache;
+		return Cache.Taille;
+	end Taille;
 
+	function Frequence(Arbre : in T_Arbre) return Integer is
+	begin
+		return Arbre.All.Frequence;
+	end Frequence;
 
+	procedure Enregistrer(Arbre : in out T_Arbre; Cache : in out T_Cache_Arbre; Adresse : in T_Adresse_IP; Masque : T_Adresse_IP; Sortie : Unbounded_String; Taille : Integer) is
+	begin
 		-- Cas où le cache est vide
-		if Est_Vide(Cache) then
-			Cache := new T_Cache_Cellule'(Taille, Adresse, Masque, Sortie, null, null, 0, False);
-			Compteur_Taille.All.Taille := Compteur_Taille.All.Taille + 1;
+		if Est_Vide(Arbre) then
+			Arbre := new T_Cache_Cellule'(Adresse, Masque, Sortie, null, null, 0, False);
+			Cache.Taille := Cache.Taille + 1;
 		else
 			Put_Line("Le cache n'est pas vide. On peut continuer.");
 		end if;
@@ -49,41 +55,38 @@ package body cache_tree is
 		for i in 0..31 loop
 			if ((Adresse AND (2 ** (31 - i))) = 0) then
 				--  Cas où le bit vaut 0
-				if Est_Vide(Cache.All.Gauche) then
+				if Est_Vide(Arbre.All.Gauche) then
 				-- Cas où le cache à gauche est vide
-					Cache.All.Gauche := new T_Cache_Cellule'(Taille, Adresse, Masque, Sortie, null, null, 0, False);
-					Compteur_Taille.All.Taille := Compteur_Taille.All.Taille + 1;
-					Cache := Cache.All.Gauche;
+					Arbre.All.Gauche := new T_Cache_Cellule'(Adresse, Masque, Sortie, null, null, 0, False);
+					Cache.Taille := Cache.Taille + 1;
+					Arbre := Arbre.All.Gauche;
 				else
-					Cache := Cache.All.Gauche;
+					Arbre := Arbre.All.Gauche;
 				end if;
 			else
 				-- Cas où le bit vaut 1
-				if Est_Vide(Cache.All.Droite) then
+				if Est_Vide(Arbre.All.Droite) then
 				-- Cas où le cache à droite est vide
-					Cache.All.Droite := new T_Cache_Cellule'(Taille, Adresse, Masque, Sortie, null, null, 0, False);
-					Compteur_Taille.All.Taille := Compteur_Taille.All.Taille + 1;
-					Cache := Cache.All.Droite;
+					Arbre.All.Droite := new T_Cache_Cellule'(Adresse, Masque, Sortie, null, null, 0, False);
+					Cache.Taille := Cache.Taille + 1;
+					Arbre := Arbre.All.Droite;
 				else
-					Cache := Cache.All.Droite;
+					Arbre := Arbre.All.Droite;
 				end if;
 			end if;
 		end loop;
 
 		-- On devrait être au niveau d'une feuille à présent
 		-- Il reste à stocker toutes les informations nécessaires
-		Cache.All.Adresse := Adresse;
-		Cache.All.Masque := Masque;
-		Cache.All.Sortie := Sortie;
-		Cache.All.Active := True;
-
-		-- On détruit le compteur pour la taille
-		Free(Compteur_Taille);
+		Arbre.All.Adresse := Adresse;
+		Arbre.All.Masque := Masque;
+		Arbre.All.Sortie := Sortie;
+		Arbre.All.Active := True;
 	end Enregistrer;
 
-	procedure Ajouter_Frequence(Cache : in out T_Cache_Arbre; Adresse : in T_Adresse_IP) is
+	procedure Ajouter_Frequence(Arbre : in out T_Arbre; Adresse : in T_Adresse_IP) is
 	begin
-		if Est_Vide(Cache) then
+		if Est_Vide(Arbre) then
 		-- Cas où le cache est vide
 			raise Adresse_Absente_Exception;
 		else
@@ -94,53 +97,50 @@ package body cache_tree is
 		for i in 0..(31 - 1) loop
 			if ((Adresse AND (2 ** (31 - i))) = 0) then
 				--  Cas où le bit vaut 0
-				if Est_Vide(Cache.Gauche) then
+				if Est_Vide(Arbre.Gauche) then
 				-- Cas où le cache à gauche est vide
 					raise Adresse_Absente_Exception;
 				else
-					Cache := Cache.All.Gauche;
+					Arbre := Arbre.All.Gauche;
 				end if;
 			else
 				-- Cas où le bit vaut 1
-				if Est_Vide(Cache.Droite) then
+				if Est_Vide(Arbre.Droite) then
 				-- Cas où le cache à droite est vide
 					raise Adresse_Absente_Exception;
 				else
-					Cache := Cache.All.Droite;
+					Arbre := Arbre.All.Droite;
 				end if;
 			end if;
 		end loop;
 
 		-- On devrait être au niveau de la feuille correspondante à l'adresse désormais
-		Cache.All.Frequence := Cache.All.Frequence + 1;
+		Arbre.All.Frequence := Arbre.All.Frequence + 1;
 	exception
 		when Adresse_Absente_Exception => Put("L'adresse demandée n'est pas présente.");
 	end Ajouter_Frequence;
 
-	procedure Supprimer(Cache : in out T_Cache_Arbre; Politique : in T_Politique; Taille : in Integer) is
-		Compteur_Taille : T_Cache_Arbre;
+	procedure Supprimer(Arbre : in out T_Arbre; Cache : in out T_Cache_Arbre; Politique : in T_Politique; Taille : in Integer) is
 
-		procedure Supprimer_FIFO(Cache : in out T_Cache_Arbre) is
+		procedure Supprimer_FIFO(Arbre : in out T_Arbre) is
 		begin
 			null; -- à compléter
-			Cache.All.Taille := Cache.All.Taille - 1;
 		end Supprimer_FIFO;
 
-		procedure Supprimer_LRU(Cache : in out T_Cache_Arbre) is
+		procedure Supprimer_LRU(Arbre : in out T_Arbre) is
 		begin
 			null; -- à compléter
-			Cache.All.Taille := Cache.All.Taille - 1;
 		end Supprimer_LRU;
 
-		function Recherche_Frequence_Min(Cache : in T_Cache_Arbre) return T_Adresse_IP is
-			Recherche_Frequence1 : T_Cache_Arbre;
-			Recherche_Frequence2 : T_Cache_Arbre;
+		function Recherche_Frequence_Min(Arbre : in T_Arbre) return T_Adresse_IP is
+			Recherche_Frequence1 : T_Arbre;
+			Recherche_Frequence2 : T_Arbre;
 			Min : Integer;
 			Adresse : T_Adresse_IP;
 		begin
 			-- On fait pointer les pointeurs sur la racine
-			Recherche_Frequence1 := Cache;
-			Recherche_Frequence2 := Cache;
+			Recherche_Frequence1 := Arbre;
+			Recherche_Frequence2 := Arbre;
 			Min := 100000; -- on n'utilisera en pratique jamais plus de 100,000 fois une adresse
 			Adresse := 0; -- par défaut
 			
@@ -170,14 +170,14 @@ package body cache_tree is
 			else
 				-- On regarde les cas où on sort des if à cause des premières conditions
 				if Recherche_Frequence1 /= null then
-					if Min > Cache.All.Frequence then
+					if Min > Arbre.All.Frequence then
 						Min := Recherche_Frequence1.All.Frequence;
 						Adresse := Recherche_Frequence1.All.Adresse;
 					else
 						null; -- il ne ne passe rien
 					end if;
 				elsif Recherche_Frequence2 /= null then
-					if Min > Cache.All.Frequence then
+					if Min > Arbre.All.Frequence then
 						Min := Recherche_Frequence2.All.Frequence;
 						Adresse := Recherche_Frequence1.All.Adresse;
 					else
@@ -191,13 +191,13 @@ package body cache_tree is
 			return Adresse;
 		end Recherche_Frequence_Min;
 
-		procedure Supprimer_LFU(Cache : in out T_Cache_Arbre) is
+		procedure Supprimer_LFU(Arbre : in out T_Arbre) is
 			Adresse : T_Adresse_IP;
-			Suppresseur : T_Cache_Arbre;
+			Suppresseur : T_Arbre;
 		begin
 			-- Il faut faire la recherche du minimum en terme de fréquence et noter son adresse (= le parcours) ainsi que créer un pointeur temporaire
-			Adresse := Recherche_Frequence_Min(Cache); -- pas d'erreur retournée étant donné que le cache est plein (il existe au moins une adresse)
-			Suppresseur := Cache;
+			Adresse := Recherche_Frequence_Min(Arbre); -- pas d'erreur retournée étant donné que le cache est plein (il existe au moins une adresse)
+			Suppresseur := Arbre;
 
 			-- On regarde pour chaque bit de l'adresse si il vaut 0 ou 1 pour savoir quelle direction prendre
 			for i in 0..(31 - 1) loop
@@ -211,18 +211,19 @@ package body cache_tree is
 			end loop;
 
 			-- Il ne reste plus qu'à supprimer cette cellule et diminiuer la taille de 1
-			Cache.All.Taille := Cache.All.Taille - 1;
 			Free(Suppresseur);
 		end Supprimer_LFU;
 
 	begin
 		-- On regarde quelle est la procédure
 		case T_Politique'Pos(Politique) is
-			-- when 1 => Supprimer_FIFO(Cache); -- FIFO : à faire (peut être)
-			-- when 2 => Supprimer_LRU(Cache); -- LRU : à faire (peut être)
-			when 3 => Supprimer_LFU(Cache); -- LFU
+			-- when 1 => Supprimer_FIFO(Arbre); -- FIFO : à faire (peut être)
+			-- when 2 => Supprimer_LRU(Arbre); -- LRU : à faire (peut être)
+			when 3 => Supprimer_LFU(Arbre); -- LFU
 			when others => raise Politique_non_valide_exception;
 		end case;
+
+		Cache.Taille := Cache.Taille - 1;
 
 	exception
 		when Politique_non_valide_exception => Put("La politique demandée n'est pas valide.");
@@ -231,7 +232,7 @@ package body cache_tree is
 	function Est_Plein(Cache : in T_Cache_Arbre; Taille : in Integer) return Boolean is
 		Est_Plein : Boolean;
 	begin
-		if Cache.All.Taille >= Taille then
+		if Cache.Taille >= Taille then
 			Est_Plein := True;
 		else
 			Est_Plein := False;
@@ -241,7 +242,7 @@ package body cache_tree is
 	end Est_Plein;
 
 	procedure Afficher_Cache(Cache : in T_Cache_Arbre) is
-		Afficheur : T_Cache_Arbre;
+		Afficheur : T_Arbre;
 	begin
 		null ; -- à compléter
 	end Afficher_Cache;
--- cache_tree.ads
+++ cache_tree.ads
@@ -4,75 +4,100 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 package cache_tree is
 
     type T_Cache_Arbre is limited private;
-
+    type T_Arbre is limited private;
 
     -- nom : Initialiser
-    -- sémantique : Initialiser le cache à initialiser
+    -- sémantique : Initialiser le cache et sa taille
     -- paramètres :
     --      Cache : Mode Out T_Cache_Arbre; -- le cache
-    -- post-condition : Est_Vide(Cache)
+    --      Taille : Mode in Integer; -- la taille du cache
+    -- post-condition : Est_Vide(Cache.Arbre)
     -- tests :
     --      entrées : . sortie : Cache = null.
-    procedure Initialiser(Cache: out T_Cache_Arbre) with
-        Post => Est_Vide(Cache);
+    procedure Initialiser(Cache : out T_Cache_Arbre; Taille : in Integer) with
+        Post => Est_Vide(Cache.Arbre);
 
-    function Est_Vide(Cache : T_Cache_Arbre) return Boolean;
+
+    -- nom : Est_Vide
+    -- sémantique : Savoir si l'arbre du cache est vide ou non
+    -- paramètres :
+    --      Arbre : Mode in T_Arbre; -- le cache
+    -- tests :
+    --      entrées : . sortie : True.
+    function Est_Vide(Arbre : in T_Arbre) return Boolean;
 
 
     -- nom : Vider
     -- sémantique : Vider entièrement le cache
     -- paramètres :
-    --      Cache : Mode In/Out T_Cache_Arbre; -- le cache à vider
+    --      Arbre : Mode In/Out T_Arbre; -- le cache à vider
     -- post-condition : Est_Vide(Cache)
     -- tests :
     --      entrées : Cache. sortie : Cache = null.
-    procedure Vider(Cache : in out T_Cache_Arbre) with
-		Post => Est_Vide(Cache);
+    procedure Vider(Arbre : in out T_Arbre) with
+		Post => Est_Vide(Arbre);
+
+
+    -- nom : Taille
+    -- sémantique : Permet d'afficher la taille du cache
+    -- paramètres :
+    --      Cache : Mode In T_Cache_Arbre; -- le cache à afficher
+    function Taille(Cache : in T_Cache_Arbre) return Integer with
+        Post => Taille'Result >= 0;
+
+    -- nom : Frequence
+    -- sémantique : Permet d'afficher la fréquence d'une cellule de l'arbre du cache
+    -- paramètres :
+    --      Arbre : Mode In T_Arbre; -- le cache à afficher
+    function Frequence(Arbre : in T_Arbre) return Integer with
+        Post => Frequence'Result >= 0;
 
 
     -- nom : Enregistrer
     -- sémantique : Enregistrer une adresse, un masque et une sortie dans le cache
     -- paramètres :
+    --      Arbre : Mode In/Out T_Arbre; -- l'arbre du cache
     --      Cache : Mode In/Out T_Cache_Arbre; -- le cache
     --      Adresse : Mode In T_Adresse_IP; -- l'adresse IP à ajouter
     --      Masque : Mode In T_Adresse_IP; -- le masque à ajouter
     --      Sortie : Mode In Unbounded_String; -- la sortie à ajouter
-    -- post-condition : Cache.All.Taille = Cache.All.Taille'Last + 1
-    procedure Enregistrer(Cache : in out T_Cache_Arbre; Adresse : in T_Adresse_IP; Masque : T_Adresse_IP; Sortie : Unbounded_String; Taille : Integer) with
-        Post => Cache.All.Taille = Cache.All.Taille'Old + 1;
+    -- post-condition : Taille(Arbre) = Taille(Arbre)'Old + 1
+    procedure Enregistrer(Arbre : in out T_Arbre; Cache : in out T_Cache_Arbre; Adresse : in T_Adresse_IP; Masque : T_Adresse_IP; Sortie : Unbounded_String; Taille : Integer) with
+        Post => Taille(Arbre) = Taille(Arbre)'Old + 1;
 
 
     -- nom : Ajouter_Frequence
     -- sémantique : Ajouter une fréquence lorsque l'on utilise une adresse dans le cache
     -- paramètres :
-    --      Cache : Mode In/Out T_Cache_Arbre; -- le cache
+    --      Arbre : Mode In/Out T_Arbre; -- l'arbre du cache
     --      Adresse : Mode In T_Adresse_IP; -- l'adresse IP utilisée
-    -- pré-condition : not Est_Vide(Cache)
+    -- pré-condition : not Est_Vide(Arbre)
     -- post-condition : Cache.All.Frequence = Cache.All.Frequence'Last + 1
-    procedure Ajouter_Frequence(Cache : in out T_Cache_Arbre; Adresse : in T_Adresse_IP) with
-        Pre => not Est_Vide(Cache),
-        Post => Cache.All.Frequence = Cache.All.Frequence'Old + 1;
+    procedure Ajouter_Frequence(Arbre : in out T_Arbre; Adresse : in T_Adresse_IP) with
+        Pre => not Est_Vide(Arbre),
+        Post => Frequence(Arbre) = Frequence(Arbre)'Old + 1;
 
 
     -- nom : Supprimer
     -- sémantique : Supprimer un élément du cache selon la politique
     -- paramètres :
-    --      Cache : Mode In/Out T_Cache_Arbre; -- le cache
+    --      Arbre : Mode In/Out T_Arbre; -- l'arbre du cache
+    --      Cache : Mode In/Out T_Cache_Arbre -- le cache
     --      Politique : Mode In T_Politique; -- la politique choisie
     -- pré-condition : Est_Plein(Cache)
-    -- post-condition : Cache.All.Taille = Cache.All.Taille'Last - 1
-    procedure Supprimer(Cache : in out T_Cache_Arbre; Politique : in T_Politique; Taille : in Integer) with
+    -- post-condition : Taille(Arbre) = Taille(Arbre)'Old - 1
+    procedure Supprimer(Arbre : in out T_Arbre; Cache : in out T_Cache_Arbre; Politique : in T_Politique; Taille : in Integer) with
         Pre => Est_Plein(Cache, Taille),
-        Post => Cache.All.Taille = Cache.All.Taille'Old - 1;
+        Post => Taille(Arbre) = Taille(Arbre)'Old - 1;
 
 
     -- nom : Est_Plein
     -- sémantique : Permet de savoir si le cache est plein ou non
     -- paramètres :
     --      Cache : Mode In T_Cache_Arbre; -- le cache
-    -- pré-condition : not Est_Vide(Cache)
+    -- pré-condition : Cache.Taille > 0
     function Est_Plein(Cache : in T_Cache_Arbre; Taille : in Integer) return Boolean with
-        Pre => not Est_Vide(Cache);
+        Pre => Taille(Cache) > 0;
 
     -- nom : Afficher_Cache
     -- sémantique : Permet d'afficher le cache
@@ -84,15 +109,18 @@ private
 
     type T_Cache_Cellule;
 
-    type T_Cache_Arbre is access T_Cache_Cellule;
+    type T_Arbre is access T_Cache_Cellule;
+    type T_Cache_Arbre is record
+        Arbre : T_Arbre;
+        Taille : Integer;
+    end record;
 
     type T_Cache_Cellule is record
-        Taille : Integer;
         Adresse : T_Adresse_IP;
         Masque : T_Adresse_IP;
         Sortie : Unbounded_String;
-        Gauche : T_Cache_Arbre;
-        Droite : T_Cache_Arbre;
+        Gauche : T_Arbre;
+        Droite : T_Arbre;
         Frequence : Integer; -- pour appliquer les politiques
         Active : Boolean; -- pour savoir si la cellule est active ou non
     end record;
