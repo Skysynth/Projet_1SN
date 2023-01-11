59c2ec2d7e6e352575367118d90124c88012b9f3
juleelee <119613908+juleelee@users.noreply.github.com>
2023-01-11 11:34:40 +0100

    test cache tree

--- test_cache_tree.adb
+++ test_cache_tree.adb
@@ -1,4 +1,9 @@
 with cache_tree; use cache_tree;
+with tools; use tools;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Text_IO; use Ada.Text_IO;
+with routeur_exceptions; use routeur_exceptions;
+with Ada.Unchecked_Deallocation;
 
 procedure test_cache_tree is
 
@@ -32,21 +37,54 @@ procedure test_cache_tree is
         return Adresse_Converti;
             
     end Convert_Unbounded_String_To_T_Adresse_IP;
--- test de la procedure initialiser 
-
--- test de la procedure Est_Vide 
 
 
--- test de la procedure Taille_cache
+-- Test de la procedure Initialiser et Est_vide
+    procedure Tester_Initiliser is 
+        Cache : T_Cache_Arbre;
+        Taille : Interger;
+    begin 
+        pragma Assert( Est_Vide(Cache));
+    
+        Initialiser(Cache, Taille => 0);
+        pragma Assert(not(Est_Vide(Cache)));
+    
+        Put_Line("Les tests de 'Initialiser' et 'Est_Vide' sont réussis !");
+
+    end Tester_Initiliser;
+
+-- Test de la procedure Enregistrer et Supprimer
+    procedure Tester_Enregistrer_Supprimer is 
+        Arbre : T_Arbre;
+        Cache : T_Cache_Arbre;
+        Adresse : T_Adresse_IP;
+        Masque : T_Adresse_IP;
+        Sortie : Unbounded_String;
+        Politique : in T_Politique
+    begin 
+
+    end Tester_Enregistrer_Supprimer;
 
 
--- test de la procedure Vider 
+-- Test procedure Afficher_Arbre 
+
+-- Test procedure Afficher_Statistiques_Cache
+
+-- Test procedure Recherche_Identifiant_Max
+
+
+-- Test de la procedure Taille_cache  
+
+
+-- Test de la procedure Vider 
+
+
 
 
 
 
 begin
-
+    Tester_Initiliser;
 
 
 end test_cache_tree;
