65b8260b5a823303847a014f17298dbf5feee732
atrtdeee <trotelalexandre@proton.me>
2023-01-14 18:05:59 +0100

    syntaxe

--- test_cache_tree.adb
+++ test_cache_tree.adb
@@ -3,48 +3,34 @@ with tools; use tools;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Text_IO; use Ada.Text_IO;
 
-
 procedure test_cache_tree is
-
-    -- fonction qui permet de convertir des adresse IP qui sont des chaine de caractere 
+    -- Fonction qui permet de convertir des adresses IP à partir de chaines de caractères
     function Convert_Unbounded_String_To_T_Adresse_IP(ligne : Unbounded_String) return T_Adresse_IP is
         Adresse_Converti : T_Adresse_IP := 0;
         mot : Unbounded_String;
         N : Integer;
         j : Integer := 1;
     begin
-            
         for i in 0..3 loop 
             mot := Null_Unbounded_String;
-                
             N := length(ligne);
-                
             while j <= N and then Element(ligne, j) /= '.' loop
-                
                 mot := mot & Element(ligne, j);
                 j := j+1;
-                    
             end loop;
-                
             -- Enregistrer la destination et le masque une fois converti dans le routeur et enregistrer l'interface dans le routeur
-              
             Adresse_Converti := Adresse_Converti + T_Adresse_IP'Value(To_String(mot)) * (2 ** (24-8*i));
-            
             j := j + 1;
-            
         end loop;
         return Adresse_Converti;
-            
     end Convert_Unbounded_String_To_T_Adresse_IP;
 
-
--- Test de la procedure Initialiser et Est_vide
-    procedure Tester_Initiliser is 
+-- Test des procédures 'Initialiser' et 'Est_vide'
+    procedure Test_Initialiser_Est_Vide is 
         Cache : T_Cache;
         Taille_Max : Integer;
         Arbre : T_Arbre;
     begin 
-
         Taille_Max := 2;
         Initialiser_Cache(Cache, Taille_Max, FIFO); -- FIFO
         Initialiser_Arbre(Arbre);
@@ -52,11 +38,10 @@ procedure test_cache_tree is
     
         Put_Line("Les tests de 'Initialiser' et 'Est_Vide' sont réussis !");
         New_Line;
+    end Test_Initialiser_Est_Vide;
 
-    end Tester_Initiliser;
-
--- Test de la procedure Enregistrer et Supprimer et Afficher
-    procedure Tester_Enregistrer_Supprimer is 
+-- Test de toutes les procédures
+    procedure Test is 
         Arbre : T_Arbre;
         Cache : T_Cache;
         Adresse1 : T_Adresse_IP;
@@ -74,12 +59,12 @@ procedure test_cache_tree is
         Min_Frequence : Integer; 
         Min_Identifiant_Frequence : Integer;
     begin 
-        -- initialiser le cache et l'arbre
-        Politique := FIFO; -- FIFO
+        -- Initialiser le cache et l'arbre
+        Politique := FIFO; -- FIFO, LRU ou LFU
         Initialiser_Cache(Cache, 3, Politique);
         Initialiser_Arbre(Arbre);
         
-        -- Enregistrement de la premiere donnees dans le cache 
+        -- Enregistrement de la première donnée dans le cache 
         Adresse1 := Convert_Unbounded_String_To_T_Adresse_IP(To_Unbounded_String("192.168.0.0"));
         Put_Line("Voici l'adresse 1 :" & T_Adresse_IP'Image(Adresse1));
         New_Line;
@@ -91,11 +76,11 @@ procedure test_cache_tree is
         New_Line;
         Afficher_Arbre(Arbre);
 
-        -- Test qui verifie si la sortie est correcte
+        -- Test qui vérifie si la sortie est correcte
         pragma Assert(Chercher_Arbre(Arbre, Cache, Adresse1) = Sortie1);
         Put_Line("L'adresse 1 a été trouvée et retourne la sortie 1 : " & To_String(Sortie1));
 
-        -- test qui verifie si le nombre de donnee dans le cache est correcte
+        -- Test qui vérifie si le nombre de données dans le cache est correcte
         pragma Assert(Enregistrement_Cache(Cache)= 1);
         Put_Line("Le nombre d'enregistrement dans le cache est de" & Integer'Image(Enregistrement_Cache(Cache)));
         New_Line;
@@ -107,7 +92,7 @@ procedure test_cache_tree is
         Sortie2 := To_Unbounded_String("eth2");
         Adresse3 := Convert_Unbounded_String_To_T_Adresse_IP(To_Unbounded_String("2.16.1.2"));
 
-        -- Ajout d'une 2eme donnee
+        -- Ajout d'une 2ème donnée
         Enregistrer(Arbre, Cache, Adresse2, Masque2, Sortie2, Politique);
         Put_Line(">>> Affichage de l'arbre <<<");
         New_Line;
@@ -120,19 +105,19 @@ procedure test_cache_tree is
         pragma Assert(Chercher_Arbre(Arbre, Cache, Adresse3) /= Sortie2);
         pragma Assert(Chercher_Arbre(Arbre, Cache, Adresse1) = Sortie1);
 
-        -- test qui verifie si le nombre de donnee dans le cache est correcte
+        -- Test qui vérifie si le nombre de données dans le cache est correcte
         pragma Assert(Enregistrement_Cache(Cache)= 2);
         Put_Line("Le nombre d'enregistrement dans le cache est de" & Integer'Image(Enregistrement_Cache(Cache)));
         New_Line;
 
-        -- Ajout d'une 3eme donnee
+        -- Ajout d'une 3ème donnée
         Enregistrer(Arbre, Cache, Adresse3, Masque1, Sortie1, Politique);
         pragma Assert(Chercher_Arbre(Arbre, Cache, Adresse3) = Sortie1);
         Put_Line(">>> Affichage de l'arbre <<<");
         New_Line;
         Afficher_Arbre(Arbre);
 
-        -- test qui verifie si le nombre de donnee dans le cache est correcte
+        -- Test qui vérifie si le nombre de données dans le cache est correcte
         pragma Assert(Enregistrement_Cache(Cache)= 3);
         Put_Line("Le nombre d'enregistrement dans le cache est de" & Integer'Image(Enregistrement_Cache(Cache)));
 
@@ -162,31 +147,12 @@ procedure test_cache_tree is
         Vider(Arbre);
 
         pragma Assert(Est_Vide(Arbre));
-        Put_Line("Les tests de 'Enregistrer' et 'Supprimer' sont réussis !");
+        Put_Line("Les tests de 'Enregistrer', 'Supprimer', 'Afficher_Arbre', 'Recherche_Identifiant_Min', 'Recherche_Identifiant_Max', 'Recherche_Frequence_Min' et 'Recherche_Identifiant_Frequence_Min' sont réussis !");
         New_Line;
-
-    end Tester_Enregistrer_Supprimer;
-
-
--- Test procedure Afficher_Arbre 
-
-
-
--- Test procedure Recherche_Identifiant_Max
-
-
--- Test de la procedure Taille_cache  
-
-
-
-
-
-
-
+    end Test;
 
 begin
-    Tester_Initiliser;
-    Tester_Enregistrer_Supprimer;
-
+    Test_Initialiser_Est_Vide;
+    Test;
 
 end test_cache_tree;
