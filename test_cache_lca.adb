92edf3770802c856c16db9b47d0f7e090086d737
Skysynth <Skysynth31@gmail.com>
2023-01-14 18:18:22 +0100

    fusion "Test_Vider" and "Test_Enregistrer"

--- test_cache_lca.adb
+++ test_cache_lca.adb
@@ -18,32 +18,20 @@ procedure TEST_CACHE_LCA is
       pragma Assert(POLITIQUE = LRU);
       Put_Line("Initialiser check");
    end Test_Initialiser;
-   
-   
-   
-   procedure Test_Vider is
-      Cache : T_CACHE_LCA;
-   begin
-      Vider(Cache);
-      pragma Assert(Est_Vide(Cache));
-      Put_Line("Est_Vide check");
-   end Test_Vider;
-   
-   
-   
-   procedure Test_Enregistrer is
+    
+   procedure Test_Enregistrer_Vider is
       Cache : T_CACHE_LCA;
       Adresse : T_Adresse_IP;
       Masque : T_Adresse_IP;
       Eth : Unbounded_String;
    begin
-      -- Initialisation de la route à mettre en cache
+      -- Initialisation de la route Ã  mettre en cache
       Initialiser(Cache, 2, LFU);
       Adresse := Unbounded_String_To_Adresse_IP(To_Unbounded_String("192.168.0.0"));
       Masque := Unbounded_String_To_Adresse_IP(To_Unbounded_String("255.255.0.0"));
       Eth := To_Unbounded_String("eth0");
       
-      -- Mise en cache de la route précédemment créée
+      -- Mise en cache de la route prÃ©cÃ©demment crÃ©Ã©e
       Enregistrer(Cache, Adresse, Masque, Eth);
       
       pragma Assert(Adresse_Presente(Cache, Adresse));
@@ -62,6 +50,10 @@ procedure TEST_CACHE_LCA is
       
       Put_Line("Enregistrer check");
       
+      Vider(Cache);
+      pragma Assert(Est_Vide(Cache));
+      Put_Line("Est_Vide check");
+      
    end Test_Enregistrer;
    
    
@@ -74,13 +66,13 @@ procedure TEST_CACHE_LCA is
       Masque_Recup : T_Adresse_IP;
       Eth_Recup : Unbounded_String;
    begin
-      -- Initialisation de la route à mettre en cache
+      -- Initialisation de la route Ã  mettre en cache
       Initialiser(Cache, 3, LFU);
       Adresse := Unbounded_String_To_Adresse_IP(To_Unbounded_String("192.168.0.0"));
       Masque := Unbounded_String_To_Adresse_IP(To_Unbounded_String("255.255.0.0"));
       Eth := To_Unbounded_String("eth0");
       
-      -- Mise en cache de la route précédemment créée
+      -- Mise en cache de la route prÃ©cÃ©demment crÃ©Ã©e
       Enregistrer(Cache, Adresse, Masque, Eth);
       
       -- Enregistrement d'une nouvelle route dans le cache
@@ -95,11 +87,11 @@ procedure TEST_CACHE_LCA is
       Eth := To_Unbounded_String("eth2");
       Enregistrer(Cache, Adresse, Masque, Eth);
       
-      -- Vérification de la fonction Est_Plein
+      -- VÃ©rification de la fonction Est_Plein
       pragma Assert(Est_Plein(Cache));
       Put_Line("Est_Plein check");
       
-      -- Récupération du masque et de l'interface dans le cache pour une adresse en particulier
+      -- RÃ©cupÃ©ration du masque et de l'interface dans le cache pour une adresse en particulier
       Adresse := Unbounded_String_To_Adresse_IP(To_Unbounded_String("192.168.1.0"));
       Masque_Recup := Recuperer_Masque_Cache(Cache, Adresse);
       Eth_Recup := Recuperer_Eth_Cache(Cache, Adresse);
@@ -119,7 +111,7 @@ procedure TEST_CACHE_LCA is
       Masque : T_Adresse_IP;
       Eth : Unbounded_String;
    begin
-      -- Initialisation de la route à mettre en cache
+      -- Initialisation de la route Ã  mettre en cache
       Initialiser(Cache, 3, FIFO);
       Adresse := Unbounded_String_To_Adresse_IP(To_Unbounded_String("192.168.0.0"));
       Masque := Unbounded_String_To_Adresse_IP(To_Unbounded_String("255.255.0.0"));
@@ -158,7 +150,7 @@ procedure TEST_CACHE_LCA is
       Masque_Recup : T_Adresse_IP;
       Eth_Recup : Unbounded_String;
    begin
-      -- Initialisation de la route à mettre en cache
+      -- Initialisation de la route Ã  mettre en cache
       Initialiser(Cache, 3, LRU);
       Adresse := Unbounded_String_To_Adresse_IP(To_Unbounded_String("192.168.0.0"));
       Masque := Unbounded_String_To_Adresse_IP(To_Unbounded_String("255.255.0.0"));
